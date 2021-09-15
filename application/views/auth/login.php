27a40d390c3c6d804e732f2b8a0a9fb434d49776
Asep rayana <asep.rayana09@gmail.com>
2021-09-15 17:36:58 +0700

    tampilan login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -4,20 +4,20 @@
   <title>Openind</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/png" href="<?php echo base_url()?>assets/login/login/images/icons/favicon.ico"/>
+  <link rel="icon" type="image/png" href="<?php echo base_url()?>assets/auth/images/icons/favicon.ico"/>
 
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/login/login/vendor/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/auth/vendor/bootstrap/css/bootstrap.min.css">
 
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/login/login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/auth/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/login/login/vendor/animate/animate.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/auth/vendor/animate/animate.css">
   
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/login/login/vendor/css-hamburgers/hamburgers.min.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/auth/vendor/css-hamburgers/hamburgers.min.css">
 
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/login/login/vendor/select2/select2.min.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/auth/vendor/select2/select2.min.css">
 
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/login/login/css/util.css">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/login/login/css/main.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/auth/css/util.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/auth/css/main.css">
 
 </head>
 <body>
@@ -26,7 +26,7 @@
     <div class="container-login100">
       <div class="wrap-login100">
         <div class="login100-pic js-tilt" data-tilt>
-          <img src="<?php echo base_url()?>assets/login/login/images/img-01.png" alt="IMG">
+          <img src="<?php echo base_url()?>assets/auth/images/img-01.png" alt="IMG">
           <!-- <img src="<?php echo base_url()?>assets/images/play-video.png" alt="play-video"> -->
           
         </div>
@@ -73,21 +73,21 @@
   
 
   
-  <script src="<?php echo base_url()?>assets/login/login/vendor/jquery/jquery-3.2.1.min.js"></script>
+  <script src="<?php echo base_url()?>assets/auth/vendor/jquery/jquery-3.2.1.min.js"></script>
 
-  <script src="<?php echo base_url()?>assets/login/login/vendor/bootstrap/js/popper.js"></script>
+  <script src="<?php echo base_url()?>assets/auth/vendor/bootstrap/js/popper.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 
-  <script src="<?php echo base_url()?>assets/login/login/vendor/select2/select2.min.js"></script>
+  <script src="<?php echo base_url()?>assets/auth/vendor/select2/select2.min.js"></script>
 
-  <script src="<?php echo base_url()?>assets/login/login/vendor/tilt/tilt.jquery.min.js"></script>
+  <script src="<?php echo base_url()?>assets/auth/vendor/tilt/tilt.jquery.min.js"></script>
   <script >
     $('.js-tilt').tilt({
       scale: 1.1
     })
   </script>
 
-  <script src="<?php echo base_url()?>assets/login/login/js/main.js"></script>
+  <script src="<?php echo base_url()?>assets/auth/js/main.js"></script>
 
 </body>
 </html>
