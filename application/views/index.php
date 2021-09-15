0ed61955e33f1924b4e413f47b5c3b251d0ef2a9
Asep rayana <asep.rayana09@gmail.com>
2021-09-15 17:29:57 +0700

    tampilan depan

--- application/views/index.php
+++ application/views/index.php
@@ -152,7 +152,7 @@
           <div class="col-lg-6 d-flex flex-column justify-content-center about-content">
 
             <div class="section-title">
-              <h2>Tentang kami</h2>
+            <img src="<?php echo base_url()?>assets/img/header3.png" class="img-fluid" alt="">
               <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.</p>
             </div>
 
@@ -269,7 +269,7 @@
       <div class="container">
 
         <div class="section-title">
-          <h2>Layanan kami</h2>
+        <img src="<?php echo base_url()?>assets/img/header1.png" class="img-fluid" alt="">
         </div>
 
         <div class="row">
@@ -291,7 +291,7 @@
       <div class="container" data-aos="fade-up" data-aos-delay="100">
 
         <div class="section-title">
-          <h2>Galeri</h2>
+        <img src="<?php echo base_url()?>assets/img/header2.png" class="img-fluid" alt="">
           <!-- <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p> -->
         </div>
 
@@ -444,7 +444,7 @@
       <div class="container">
 
         <div class="section-title">
-          <h2>Artikel</h2>
+        <img src="<?php echo base_url()?>assets/img/header4.png" class="img-fluid" alt="">
           <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem.</p>
         </div>
 
@@ -481,53 +481,41 @@
       <div class="container">
 
         <div class="section-title">
-          <h2>Frequently Asked Questions</h2>
+          <!-- <h2>Frequently Asked Questions</h2> -->
+          <img src="<?php echo base_url()?>assets/img/header5.png" class="img-fluid" alt="">
+
         </div>
 
         <div class="row  d-flex align-items-stretch">
 
-          <div class="col-lg-6 faq-item" data-aos="fade-up">
-            <h4>Non consectetur a erat nam at lectus urna duis?</h4>
-            <p>
-              Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
-            </p>
-          </div>
+          
+          <div class="container">
+  <br>
+  <!-- Nav tabs -->
+  <ul class="nav nav-tabs" role="tablist">
+   
+    <li class="nav-item">
+      <a class="nav-link active" data-toggle="tab" href="#menu1">Makanan</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="tab" href="#menu2">Minuman</a>
+    </li>
+  </ul>
 
-          <div class="col-lg-6 faq-item" data-aos="fade-up" data-aos-delay="100">
-            <h4>Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque?</h4>
-            <p>
-              Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim.
-            </p>
-          </div>
-
-          <div class="col-lg-6 faq-item" data-aos="fade-up" data-aos-delay="200">
-            <h4>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi?</h4>
-            <p>
-              Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus.
-            </p>
-          </div>
-
-          <div class="col-lg-6 faq-item" data-aos="fade-up" data-aos-delay="300">
-            <h4>Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla?</h4>
-            <p>
-              Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim.
-            </p>
-          </div>
-
-          <div class="col-lg-6 faq-item" data-aos="fade-up" data-aos-delay="400">
-            <h4>Tempus quam pellentesque nec nam aliquam sem et tortor consequat?</h4>
-            <p>
-              Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in
-            </p>
-          </div>
-
-          <div class="col-lg-6 faq-item" data-aos="fade-up" data-aos-delay="500">
-            <h4>Tortor vitae purus faucibus ornare. Varius vel pharetra vel turpis nunc eget lorem dolor?</h4>
-            <p>
-              Laoreet sit amet cursus sit amet dictum sit amet justo. Mauris vitae ultricies leo integer malesuada nunc vel. Tincidunt eget nullam non nisi est sit amet. Turpis nunc eget lorem dolor sed. Ut venenatis tellus in metus vulputate eu scelerisque.
-            </p>
-          </div>
-
+  <!-- Tab panes -->
+  <div class="tab-content">
+    
+    <div id="menu1" class="container tab-pane active"><br>
+      <h3>Menu 1</h3>
+      <p>Ikan Asin</p>
+    </div>
+    <div id="menu2" class="container tab-pane fade"><br>
+      <h3>Menu 2</h3>
+      <p>Teh manis.</p>
+    </div>
+  </div>
+</div>
+          
         </div>
 
       </div>
@@ -538,7 +526,7 @@
       <div class="container">
 
         <div class="section-title">
-          <h2>Contact Us</h2>
+        <img src="<?php echo base_url()?>assets/img/header6.png" class="img-fluid" alt="">
         </div>
 
         <div class="row">
